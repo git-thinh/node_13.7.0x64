1766d202ef8d5232d3fd5464515ca6a67c367251
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2020-02-01 17:27:24 +0700

    A

--- install-win-service.js
+++ install-win-service.js
@@ -2,10 +2,10 @@
 
 // Create a new service object
 var svc = new Service({
-    name: 'Hello World',
+    name: 'PROXY 12345',
     description: 'The nodejs.org example web server.',
     //script: 'C:\\path\\to\\test-hello.js',
-    script: require('path').join(__dirname, 'test-hello.js'),
+    script: require('path').join(__dirname, 'proxy.js'),
     nodeOptions: [
         '--harmony',
         '--max_old_space_size=4096'
